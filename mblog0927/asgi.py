8fbefacad62472efd3403b3e8c503f98740657f1
leonjyentub <leonjye@ntub.edu.tw>
2023-09-27 14:55:28 +0800

    hello djangooooooo

---
+++ mblog0927/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mblog0927 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog0927.settings')
+
+application = get_asgi_application()
