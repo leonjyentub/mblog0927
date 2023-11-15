6143f9d6c9d597c92cafee30249a2f06b56673e7
leonjye <leonjye@ntub.edu.tw>
2023-11-15 14:39:25 +0800

    modified about

--- mysite/views.py
+++ mysite/views.py
@@ -16,14 +16,14 @@ def showpost(request, slug):
     
 import random
 def about(request, num=-1):
-    if num == -1 or num > 3:
-        quotes = ['今日事，今日畢',
-                '要怎麼收穫，先那麼栽',
-                '知識就是力量',
-                '一個人的個性就是他的命運']
+    quotes = ['今日事，今日畢',
+              '要怎麼收穫，先那麼栽',
+              '知識就是力量',
+              '一個人的個性就是他的命運']
+    if num == -1 or num > 4:
         quote = random.choice(quotes)
     else:
-        quotes = num
+        quote = quotes[num]
     return render(request, 'about.html', locals())   
 
 
