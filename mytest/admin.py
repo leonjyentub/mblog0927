6003523ab05d4cbbc0c649d5fd3d56c77e19eb45
leonjye <leonjye@ntub.edu.tw>
2023-12-13 15:11:36 +0800

    add new app mytest to project add new models: Post, Mood

--- mytest/admin.py
+++ mytest/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from mytest.models import Post, Mood
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('nickname','message','del_pass', 'pub_time', 'enabled')
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Mood)
