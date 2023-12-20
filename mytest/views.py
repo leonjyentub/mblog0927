d6d53d0d28c4cfaa24a37de26fdd236263e86310
leonjye <leonjye@ntub.edu.tw>
2023-12-20 14:02:54 +0800

    add delete action & icon

--- mytest/views.py
+++ mytest/views.py
@@ -8,19 +8,15 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'myform.html', locals())
     elif request.method == 'POST':
-        try:
-            user_id = request.POST['user_id']
-            user_pass = request.POST['user_pass']
-            user_post = request.POST['user_post']
-            user_mood = request.POST['mood']
-            mood = Mood.objects.get(status=user_mood)
-            post = Post(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
-            post.save()
-            message = f'成功儲存！請記得你的編輯密碼[{user_pass}]!，訊息需經審查後才會顯示。'
-            return render(request, 'myform.html', locals())
-        except Exception as e:
-            message = '出現錯誤'
-            return render(request, 'myform.html', locals())
+        user_id = request.POST['user_id']
+        user_pass = request.POST['user_pass']
+        user_post = request.POST['user_post']
+        user_mood = request.POST['mood']
+        mood = Mood.objects.get(status=user_mood)
+        post = Post(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+        post.save()
+        message = f'成功儲存！請記得你的編輯密碼[{user_pass}]!，訊息需經審查後才會顯示。'
+        return render(request, 'myform.html', locals())
     else:
         message = 'post/get 出現錯誤'
         return render(request, 'myform.html', locals())
